cf3e10f2b5b9a1450d16f3d32da760efd9eaefeb
rafaaacampelo <rafaccdfranca@gmail.com>
2025-02-25 14:42:23 -0300

    vapo

--- modulos e importações/listas.py
+++ modulos e importações/listas.py
@@ -1,1 +1,15 @@
-# em proccessoooooo
+# um professor quer sortear um dos seus alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
+# o msm professor vai sortear a ordem de apresentação dos alunos. faça um programa que leia o nome dos quaro alunos e mostre a ordem sorteada.
+
+import random
+n1 = str(input('primeiro aluno:'))
+n2 = str(input('segundo aluno:'))
+n3 = str(input('terceiro aluno:'))
+n4 = str(input('quarto aluno:'))
+
+lista = [n1, n2, n3, n4]
+escolhido = random.choice(lista)
+ordem = random.shuffle(lista)
+print('o aluno escolhido foi {}' .format(escolhido))
+print('a ordem de apresentação é')
+print(lista)
