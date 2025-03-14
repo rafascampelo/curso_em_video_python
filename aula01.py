e96e763238ee9737f9bf80fd6445c9dcaaa790b0
aluno cmc <alunocmc@umc.br>
2025-03-13 21:38:54 -0300

    foi

--- aula01.py
+++ aula01.py
@@ -1,20 +1,22 @@
-print("escolha uma função:")
-print("1. olá, mundo!")
-print("2. Quem é você")
-print("3. que horas são?")
-print("4. tchau")
+print("Bem-vindo!!")
+print("Escolha uma função:")
+print("1. Quem é você")
+print("2. Que horas são?")
+print("3. Quem sou eu?")
+print("4. Tchau")
 
 while True:
     opcao = input("digite o numero da opção desejada:")
     if opcao == '1':
-        print ('olá mundo!')
+        resposta1 = input("quem é você?")
+        print("Então você é : {}" .format(resposta1))
     elif opcao == '2':
-        resposta2 = input("quem é você?")
-        print("Então você é : {}" .format(resposta2))
-    elif opcao == '3':
         from datetime import datetime
         agora = datetime.now()
         print("A hora atual é:", agora.strftime("%H:%M:%S"))
+    elif opcao == '3':
+       resposta3 = input("quem sou eu?")
+       
     elif opcao == '4':
         print("saindo..")
         break
